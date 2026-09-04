0911ad1c888fcfc5120760b7d94d81ee6af8ed80
jdwebs15 <dweber@nortonpanthers.org>
2026-09-04 12:58:46 -0400

    Update app.js

--- worksheets/basic-principles-constitution-intro/app.js
+++ worksheets/basic-principles-constitution-intro/app.js
@@ -1,5 +1,5 @@
 /* Paste the Government Hub's existing Apps Script /exec URL here. */
-const SCRIPT_URL="https://script.google.com/macros/s/AKfycbx_5OGAlYAWDEHZyRGGT71dBwd-C8PPfIMOOOPghs2W0pBtfFy8eG5kSU63f4fecLuk/exec";
+const SCRIPT_URL="https://script.google.com/macros/s/AKfycbyAkahbkT3Tn7wVxAap3EqF1uhnB2iJF9hm6pnuUueHkBibHUgZg2R8ZVEHE-Gky6y7/exec";
 const META={title:"Basic Principles of the Constitution",subtitle:"Introductory Short-Answer Worksheet",assignmentKey:"basic-principles-constitution-2026-v1"};
 
 const URL={
